0ecf57fb54c131043124727e1e0cd2d4700e0043
James Galvis <jamesrgal@gmail.com>
2024-09-19 18:30:22 -0500

    Google OAuth Implemented

--- auth.config.ts
+++ auth.config.ts
@@ -9,8 +9,8 @@ import { getUserByEmail } from "@/actions/user"
 export default {
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
       authorization: {
         params: {
           prompt: "consent",
